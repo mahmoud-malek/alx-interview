3ebc94278b5ae7f89b01dc77f4192cc3726451a7
mahmoud malek <information.mahmoud@gmail.com>
2024-09-06 04:10:17 +0300

    add\udpate files :fire:

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-
-""" this is a module for solving problems """
+"""
+Defines a UTF-8 Validation function
+"""
 
 
 def validUTF8(data):
-    """ a function that determines if
-     a given data set represents a valid UTF-8 encoding """
-    count = 0
-    for byte in data:
-        if count == 0:
-            if (byte >> 5) == 0b110:
-                count = 1
-            elif (byte >> 4) == 0b1110:
-                count = 2
-            elif (byte >> 3) == 0b11110:
-                count = 3
-            elif (byte >> 7):
+    """
+    UTF-8 Validation
+    Args:
+        data (list[int]): an array of characters represented as 1byte int
+    Returns:
+        (True): if all characters in data are valid UTF-8 code point
+        (False): if one or more characters in data are invalid code point
+    """
+    msb_mask = 1 << 7
+    second_msb_mask = 1 << 6
+    byte_count = 0
+    for code_point in data:
+        leading_bit_mask = 1 << 7
+        if byte_count == 0:
+            while leading_bit_mask & code_point:
+                byte_count += 1
+                leading_bit_mask = leading_bit_mask >> 1
+            if byte_count == 0:
+                continue
+            if byte_count == 1 or byte_count > 4:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            if not (code_point & msb_mask and not (
+                    code_point & second_msb_mask)):
                 return False
-            count -= 1
-    return count == 0
+        byte_count -= 1
+    return byte_count == 0
