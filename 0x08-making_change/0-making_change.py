69afb0bbd12c88c74a057635481ef0a59c8cdd72
mahmoud malek <information.mahmoud@gmail.com>
2024-10-04 04:44:24 +0300

    add\udpate files :fire:

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,8 @@
 
 def makeChange(coins, total):
     """ Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total """
+    determine the fewest number of coins needed
+         to meet a given amount total """
     if total <= 0:
         return 0
 
